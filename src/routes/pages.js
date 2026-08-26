7adc2efd1cef79fc2573811c3bc9a69284696625
letsopen <letsopen@live.com>
2026-08-26 09:58:36 +0800

    移除死代码

--- src/routes/pages.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const publicController = require('../controllers/publicController');
-
-const router = express.Router();
-
-router.get('/', publicController.home);
-router.get('/website/:id', publicController.detail);
-
-module.exports = router;
